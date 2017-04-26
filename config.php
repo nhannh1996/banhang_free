bfbc4beefb1ff18d86a36f5ff36929590eac278d
nhannh1996 <nhannh1996@gmail.com>
2017-04-26 11:40:49 +0700

    up new file

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+$hostname     = "mysql.hostinger.vn";
+$username     = "u484818263_nhan";
+$password     = "nhannhaN13";
+$databasename = "u484818263_db";
+$visitorTimeout = 900; // =15 * 60 Download source mien phi tai Sharecode.vn
+
+$MAXPAGE = 10;
+$multiLanguage = 1;//0 : single  ;  1 : multi
+
+$arrLanguage = array(
+	array('vn','Việt Nam'),
+	array('en','English')
+
+);
+
+?>
+
